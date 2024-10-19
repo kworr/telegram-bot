4cecaa175e5b81ac091763f20b7a2dbeed40600e
Yotam Ofek <yotam.ofek@gmail.com>
2024-10-19 11:24:07 +0000

    [lib] small cleanup that's a weird use of `Result`'s being an iterator

--- lib/src/connector/hyper.rs
+++ lib/src/connector/hyper.rs
@@ -141,14 +141,10 @@ impl<C: Connect + std::fmt::Debug + 'static + Clone + Send + Sync> Connector for
             .map_err(Error::from)?;
 
             let response = client.request(request).await.map_err(Error::from)?;
-            let whole_chunk = to_bytes(response.into_body()).await;
-
-            let body = whole_chunk
-                .iter()
-                .fold(vec![], |mut acc, chunk| -> Vec<u8> {
-                    acc.extend_from_slice(chunk);
-                    acc
-                });
+            let body = to_bytes(response.into_body())
+                .await
+                .unwrap_or_default()
+                .to_vec();
 
             Ok::<HttpResponse, Error>(HttpResponse { body: Some(body) })
         };
